1651f88ac33b91b2e851d8e151bb4b97b4e76d46
Ayrton <devayrtonsbarreto@gmail.com>
2022-04-04 12:19:24 -0300

    Colocando duas exceções no mesmo catch

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -4,7 +4,7 @@ public class Fluxo {
         System.out.println("Ini do main");
         try {
             metodo1();
-        }catch (ArithmeticException ex){
+        }catch (ArithmeticException | NullPointerException ex){
             //String msg = ex.getMessage();
             //System.out.println("ArithmeticException " + msg);
             ex.printStackTrace();
@@ -24,6 +24,7 @@ public class Fluxo {
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
                 //int a = i/0;
+
                 Conta c = null;
                 c.deposita();
         }
